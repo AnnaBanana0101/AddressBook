6db57fd1cbc8ae921a139b133e1855cd3d534811
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-21 17:40:52 +0530

    Implemented UC2

--- AddressBook.java
+++ AddressBook.java
@@ -1,52 +1,19 @@
 package addressbook;
-
-import java.util.Scanner;
+import java.util.*;
 import java.util.ArrayList;
 
 public class AddressBook {
 
     ArrayList<Contact> contactList = new ArrayList<>();
 
-    public void createContact()
-    {
-
-        String first_name;
-        String last_name;
-        String address;
-        String city;
-        String state;
-        String zip;
-        String phone_number;
-        String email;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter First Name: ");
-        first_name = sc.nextLine();
-
-        System.out.println("Enter Last Name: ");
-        last_name = sc.nextLine();
-
-        System.out.println("Enter Address: ");
-        address = sc.nextLine();
-
-        System.out.println("Enter City: ");
-        city = sc.nextLine();
-
-        System.out.println("Enter State: ");
-        state = sc.nextLine();
-
-        System.out.println("Enter ZIP Code: ");
-        zip = sc.nextLine();
-
-        System.out.println("Enter Phone Number: ");
-        phone_number = sc.nextLine();
-
-        System.out.println("Enter Email ID: ");
-        email = sc.nextLine();
+    public void createContact(String first_name, String last_name, String address, String city, String state, String zip, String phone_number, String email) {
 
         Contact contact = new Contact(first_name, last_name, address, city, state, zip, phone_number, email);
 
         contactList.add(contact);
+    }
+
+    public void displayBook() {
+        System.out.println(contactList);
     }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,64 @@
 package addressbook;
 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Management!");
+
+        Scanner sc = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+
+
+        String first_name;
+        String last_name;
+        String address;
+        String city;
+        String state;
+        String zip;
+        String phone_number;
+        String email;
+        int done = 0;
+
+        do
+        {
+            System.out.println("Enter First Name: ");
+            first_name = sc.nextLine();
+
+            System.out.println("Enter Last Name: ");
+            last_name = sc.nextLine();
+
+            System.out.println("Enter Address: ");
+            address = sc.nextLine();
+
+            System.out.println("Enter City: ");
+            city = sc.nextLine();
+
+            System.out.println("Enter State: ");
+            state = sc.nextLine();
+
+            System.out.println("Enter ZIP Code: ");
+            zip = sc.nextLine();
+
+            System.out.println("Enter Phone Number: ");
+            phone_number = sc.nextLine();
+
+            System.out.println("Enter Email ID: ");
+            email = sc.nextLine();
+            
+            //Create/add a contact to address book
+            addressBook.createContact(first_name, last_name, address, city, state, zip, phone_number, email);
+
+            System.out.println("Are you done? 0 for no, 1 for yes: ");
+            done = sc.nextInt();
+    
+        }while(done!= 1);
+
+        //Display the book
+        addressBook.displayBook();
+        
+
     }
 }
